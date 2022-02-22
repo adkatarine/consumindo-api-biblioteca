37414b6a6b0bf9d684f0a38804ea11c4771d76ca
adkatarine <adllakatarine@hotmail.com>
2022-02-22 11:52:31 -0300

    add: Adiciona consumo da api biblioteca usando a rota que insere uma obra.

--- src/blueprints/library/library.py
+++ src/blueprints/library/library.py
@@ -1,19 +1,28 @@
-from flask import Blueprint, render_template
-from src.consume_api.consume_api_library import get_works
+from flask import Blueprint, render_template, redirect, url_for, request
+from src.consume_api.consume_api_library import get_works, post_work
 
 # from flask_cors import cross_origin
 
 library_blueprint = Blueprint(
-    "library",
-    __name__,
-    template_folder="templates",
-    static_folder="static",
-    static_url_path="/library/static",
+    "library", __name__, template_folder="templates", static_folder="static"
 )
 
 
-@library_blueprint.route("/")
+@library_blueprint.route("/", methods=["GET"])
 # @cross_origin(origin='localhost', headers=['Content- Type', 'Authorization'])
 def index():
     works = get_works()
     return render_template("index.html", works=works)
+
+
+@library_blueprint.route("/create", methods=["POST"])
+def create():
+    work = {
+        "title": request.form["insert-title"],
+        "publishing_company": request.form["insert-publishing_company"],
+        "photo": request.form["insert-photo"],
+        "authors": request.form["insert-authors"].split(", "),
+    }
+    post_work(work)
+    works = get_works()
+    return redirect(url_for("library.index", works=works))
--- src/consume_api/consume_api_library.py
+++ src/consume_api/consume_api_library.py
@@ -5,3 +5,7 @@ import json
 def get_works():
     request = requests.get("http://127.0.0.1:8000/obras")
     return json.loads(request.content)
+
+
+def post_work(work):
+    requests.post("http://127.0.0.1:8000/obras", data=json.dumps(work))
